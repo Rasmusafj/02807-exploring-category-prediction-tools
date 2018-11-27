53d5fdabfddb067b43bb8fb9e466ab85b02728f8
Sabine Hansen <sabinehansen@sabines-mbp.eduroam.wireless.dtu.dk>
2018-11-27 17:04:48 +0100

    Tuning of SVM

--- models.py
+++ models.py
@@ -251,7 +251,7 @@ class SVCMachineLearningModel(AbstractModel):
         args_dict = kwargs
         args_dict["preprocessing_method"] = "hashing_vectorize"
         super().__init__(**args_dict)
-        self.fit_data()
+        #self.fit_data()
 
     def fit_data(self):
 
